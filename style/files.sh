dbb685036b63061a7a73aa2b1215a531cfc8a234
Zachary Lentz <zlentz@slac.stanford.edu>
2020-04-28 16:09:54 -0700

    CI: update to latest best CI practices

--- style/files.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find lcls-twincat-motion -regextype posix-extended -regex '.*\.(TcPOU|TcDUT)$' | grep -v tc_mca_std_lib
--- style/fix_style.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-style/files.sh | xargs sed -i -e 's/\t/    /g'
-style/files.sh | xargs sed -i -e 's/\s\+$//g'
